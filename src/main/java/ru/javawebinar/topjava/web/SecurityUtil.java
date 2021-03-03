3647f75e7cfddaa48eea56525a45a08f41131c7c
Taras Yatsura <jatsura.taras@gmail.com>
2021-03-03 17:06:16 +0200

    selecting users in html

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -4,14 +4,17 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil
 {
+    private static int id = 1;
 
-    public static int authUserId()
-    {
-        return 1;
+    public static int authUserId() {
+        return id;
     }
 
-    public static int authUserCaloriesPerDay()
-    {
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
+    }
+
+    public static int authUserCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -15,10 +15,18 @@ public class UserServlet extends HttpServlet
     private static final Logger log = getLogger(UserServlet.class);
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         log.debug("redirect to users");
         //        request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        response.sendRedirect("meals");
     }
 }
