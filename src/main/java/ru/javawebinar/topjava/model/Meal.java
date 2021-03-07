44eeb17399fa6214502ea7026bbfba6b316dfb3e
Taras Yatsura <jatsura.taras@gmail.com>
2021-03-07 14:58:56 +0200

    HW3 realization: adding init and populate of meals table; realization of meal jdbc repository

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -6,16 +6,18 @@ import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity
 {
-    private final LocalDateTime dateTime;
-    private final String description;
-    private final int calories;
+    private LocalDateTime dateTime;
+    private String description;
+    private int calories;
+
+    public Meal() {
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories)
-    {
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
@@ -42,19 +44,28 @@ public class Meal extends AbstractBaseEntity
         return dateTime.toLocalDate();
     }
 
-    public LocalTime getTime()
-    {
+    public LocalTime getTime() {
         return dateTime.toLocalTime();
     }
 
-    public boolean isNew()
-    {
+    public boolean isNew() {
         return id == null;
     }
 
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Meal{" + "id=" + id + ", dateTime=" + dateTime + ", description='" + description + '\'' +
                ", calories=" + calories + '}';
     }
