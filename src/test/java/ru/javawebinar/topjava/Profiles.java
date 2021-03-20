ee103eb4c7d22abd9a20f0df749a217bdfdaf704
Taras Yatsura <jatsura.taras@gmail.com>
2021-03-20 18:11:48 +0200

    Adding springJPAData repos implementation

--- src/test/java/ru/javawebinar/topjava/Profiles.java
+++ src/test/java/ru/javawebinar/topjava/Profiles.java
@@ -6,9 +6,9 @@ import org.springframework.util.ClassUtils;
 
 public class Profiles
 {
-    public static final String JDBC = "jdbc", JPA = "jpa";
+    public static final String JDBC = "jdbc", JPA = "jpa", DATAJPA = "datajpa";
 
-    public static final String REPOSITORY_IMPLEMENTATION = JPA;
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
 
     public static final String POSTGRES_DB = "postgres", HSQL_DB = "hsqldb";
 
