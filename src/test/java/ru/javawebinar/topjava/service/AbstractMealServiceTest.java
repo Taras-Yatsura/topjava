3575351e764a0a50eb3293a535226f95f184eca8
Taras Yatsura <jatsura.taras@gmail.com>
2021-03-29 09:04:46 +0300

    6.01 - HW3_1 - dataJpaMealRepo 6.02 - HW3_2 - tests for implemented with profiles 6.03 - HW3_3 - Time rules in external class

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -1,35 +1,21 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.Stopwatch;
-import org.junit.runner.Description;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertThrows;
-import static org.slf4j.LoggerFactory.getLogger;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration({"classpath:spring/spring-app.xml", "classpath:spring/spring-db.xml"})
+/*@ContextConfiguration({"classpath:spring/spring-app.xml", "classpath:spring/spring-db.xml"})
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql",
      config = @SqlConfig(encoding = "UTF-8"))
@@ -54,17 +40,19 @@ public class MealServiceTest
             results.append(result);
             log.info(result + " ms\n");
         }
-    };
+    };*/
+public abstract class AbstractMealServiceTest extends AbstractServiceTest
+{
 
 
     @Autowired
     private MealService service;
 
-    @AfterClass
+/*    @AfterClass
     public static void printResult() {
         log.info("\n---------------------------------" + "\nTest                 Duration, ms" +
                  "\n---------------------------------" + results + "\n---------------------------------");
-    }
+    }*/
 
     @Test
     public void delete() {
