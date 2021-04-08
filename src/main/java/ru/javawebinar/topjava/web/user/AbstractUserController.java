46f14aebb7c3fc41294ff68b776f59ccb59f54b2
Taras Yatsura <jatsura.taras@gmail.com>
2021-04-08 12:14:14 +0300

    Lesson 9 [Spring Binding]: - move ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY to ru.javawebinar.topjava.util.UserUtil; - created UserTo object with field, that send to user and receive from him (id, name, email, password); - added UserUtil with transform method from UserTo to User; - added void create(UserTo userTo) to AbstractUserController; - changed create method (remove @RequestParam, add UserTo without @RequestBody - Spring Binding 'Black magic').

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.UserUtil;
 
 import java.util.List;
 
@@ -18,27 +20,28 @@ public abstract class AbstractUserController
     @Autowired
     private UserService service;
 
-    public List<User> getAll()
-    {
+    public List<User> getAll() {
         log.info("getAll");
         return service.getAll();
     }
 
-    public User get(int id)
-    {
+    public User get(int id) {
         log.info("get {}", id);
         return service.get(id);
     }
 
-    public User create(User user)
-    {
+    public void create(UserTo userTo) {
+        log.info("create from to {}", userTo);
+        create(UserUtil.createNewFromTo(userTo));
+    }
+
+    public User create(User user) {
         log.info("create {}", user);
         checkNew(user);
         return service.create(user);
     }
 
-    public void delete(int id)
-    {
+    public void delete(int id) {
         log.info("delete {}", id);
         service.delete(id);
     }
