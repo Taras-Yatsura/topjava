3826bd7bd00cd9c9fb01deef0763bd6f3f805db6
Taras Yatsura <jatsura.taras@gmail.com>
2021-05-13 17:22:46 +0300

    HW10_optional [Duplicated email in User and dateTime in Meal error handling]: - added i18n key for meal dateTime duplicate; - added to spring-mvc.xml new bean MessageSourceAccessor that use in constructor messageSource; - removed constant EXCEPTION_DUPLICATE_EMAIL from AbstractUserController; - added EXCEPTION_DUPLICATE_EMAIL constants to ProfileUIController; - to ExceptionInfoHandler:   + added EXCEPTION_DUPLICATE_EMAIL and EXCEPTION_DUPLICATE_DATETIME constants, that use i18n values;   + added CONSTRAINS_I18N_MAP constant with keys users_unique_email_idx and meals_unique_user_datetime_idx, that use two constants;   + added autowired with constructor MessageSourceAccessor and changed conflict method to handle dublicate email and datetime; - removed catching DataIntegrityViolationException in AdminUIController - now it handled in ExceptionInfoHandler; - changed AbstractControllerTest:   + added autowired using field Environment and MessageSourceAccessor;   + added getMessage() methdo - return from MessageSourceAccessor message using code in args;   + added ResultMatcher errorType(ErrorType type) and ResultMatcher detailMessage(String code) that use jsonPath("$.type") and jsonPath("$.details") respectively - AdminRestControllerTest, MealRestControllerTest and ProfileRestController in createInvalid() and updateInvalid() methods now use AbstractControllerTest.errorType() method instead jsonPath("$.type"); - added updateDuplicate() and createDuplicate() methods to AdminRestControllerTest and MealRestControllerTest, updateDuplicate() method to ProfileRestController.
    NOTE: realization using CONSTRAINS_I18N_MAP depends on postgres

--- src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -1,15 +1,10 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.UserTo;
-import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -19,9 +14,6 @@ import java.util.List;
                 produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdminUIController extends AbstractUserController
 {
-
-    @Autowired
-    private MessageSource messageSource;
 
     @Override
     @GetMapping
@@ -45,17 +37,11 @@ public class AdminUIController extends AbstractUserController
     @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void createOrUpdate(@Valid UserTo userTo) {
-        try {
-            if (userTo.isNew()) {
-                super.create(userTo);
-            }
-            else {
-                super.update(userTo, userTo.id());
-            }
+        if (userTo.isNew()) {
+            super.create(userTo);
         }
-        catch (DataIntegrityViolationException e) {
-            throw new IllegalRequestDataException(
-                    messageSource.getMessage(EXCEPTION_DUPLICATE_EMAIL, null, LocaleContextHolder.getLocale()));
+        else {
+            super.update(userTo, userTo.id());
         }
     }
 
