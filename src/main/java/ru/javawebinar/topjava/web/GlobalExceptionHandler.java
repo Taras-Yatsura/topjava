6ba52c79305115b815c3668c8107bdde2336048f
Taras Yatsura <jatsura.taras@gmail.com>
2021-05-14 09:06:14 +0300

    Lesson 11 [404 Error handling]: - deleted default-servlet-handler from spring-mvc.xml; - deleted test.html; - added init param for main web servlet throwExceptionIfNoHandlerFound=true (handle 404 in app); - added new error type to ErrorType enum - WRONG_REQUEST("error.wrongRequest"); - added new ExceptionHandler for NoHandlerFoundException to GlobalExceptionHandler - refactor GlobalExceptionHandler - general functionality in logAndGetExceptionView();

--- src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.util.ValidationUtil;
 import ru.javawebinar.topjava.util.exception.ErrorType;
@@ -25,17 +26,20 @@ public class GlobalExceptionHandler
         this.messageSourceAccessor = messageSourceAccessor;
     }
 
-    @ExceptionHandler(Exception.class)
-    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-        log.error("Exception at request " + req.getRequestURL(), e);
-        Throwable rootCause = ValidationUtil.logAndGetRootCause(log, req, e, true, ErrorType.APP_ERROR);
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ModelAndView wrongRequest(HttpServletRequest req, NoHandlerFoundException e) throws Exception {
+        return logAndGetExceptionView(req, e, false, ErrorType.WRONG_REQUEST, HttpStatus.BAD_REQUEST);
+    }
 
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    private ModelAndView logAndGetExceptionView(HttpServletRequest req, Exception e, boolean logException,
+                                                ErrorType errorType, HttpStatus httpStatus) {
+        Throwable rootCause = ValidationUtil.logAndGetRootCause(log, req, e, logException, errorType);
+
         ModelAndView mav = new ModelAndView("exception", Map.of("exception", rootCause, "message",
                                                                 ValidationUtil.getMessage(rootCause), "typeMessage",
                                                                 messageSourceAccessor
-                                                                        .getMessage(ErrorType.APP_ERROR.getErrorCode()),
-                                                                "status", httpStatus));
+                                                                        .getMessage(errorType.getErrorCode()), "status",
+                                                                httpStatus));
         mav.setStatus(httpStatus);
 
         // Interceptor is not invoked, put userTo
@@ -44,5 +48,11 @@ public class GlobalExceptionHandler
             mav.addObject("userTo", authorizedUser.getUserTo());
         }
         return mav;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        log.error("Exception at request " + req.getRequestURL(), e);
+        return logAndGetExceptionView(req, e, true, ErrorType.APP_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
